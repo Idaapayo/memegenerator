35a0744f7c7848213b42d4df91704740f8980284
idaapayo <idaapayo@gmail.com>
2022-05-24 17:06:44 +0300

    fetching memes from api and final touches

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,37 @@
 import "../static/css/form.css"
 import "../memesdata"
-import memesdata from "../memesdata";
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
 
 export default function Form(){
-    const [memeImage, setMemeImage] = useState("")
+    const [memeData, setMemeData] = useState({
+        topText : "",
+        bottomText : "",
+        randomImage : "http://i.imgflip.com/1bij.jpg"
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState([])
 
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(data => setAllMemeImages(data.data.memes))
+    }, [])
+
+
+
+    console.log(allMemeImages)
 
     function getRandomImage(){
-        let imageArray = memesdata.data.memes.map(a => a.url)
+        let imageArray = allMemeImages.map(a => a.url)
         const randomNumber = Math.floor(Math.random() * imageArray.length)
         const randomUrl = imageArray[randomNumber]
-        setMemeImage(randomUrl)
-        console.log(randomUrl)
+        setMemeData(prevMemeData => ({ ...prevMemeData ,randomImage:randomUrl}))
+        // console.log(randomUrl)
+    }
+
+    function handleChange(event){
+        const{name, value} = event.target
+        setMemeData(prevMemeDate => ({...prevMemeDate, [name]: value}))
     }
 
     return(
@@ -22,16 +40,36 @@ export default function Form(){
                 <div className="row">
                     <div className="col-6">
                         <div className="mb-3">
-                            <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="top-text"/>
+                            <input type="text"
+                                   className="form-control"
+                                   id="exampleInputEmail1"
+                                   placeholder="top-text"
+                                   value={memeData.topText}
+                                   name="topText"
+                                   onChange={handleChange}
+                            />
+
                         </div>
                     </div>
                     <div className="col-6">
                         <div className="mb-3">
-                            <input type="text" className="form-control" id="exampleInputPassword1" placeholder="bottom-text"/>
+                            <input type="text"
+                                   className="form-control"
+                                   id="exampleInputPassword1"
+                                   placeholder="bottom-text"
+                                   value={memeData.bottomText}
+                                   name="bottomText"
+                                   onChange={handleChange}
+                            />
                         </div>
                     </div>
                 </div>
-                <img className="img-resize pt-2 pb-3" src={memeImage} alt="memeImage"/>
+                <div className="meme">
+                    <img className="img-resize pt-2 pb-3" src={memeData.randomImage} alt="memeImage"/>
+                    <h2 className="meme--text top">{memeData.topText}</h2>
+                    <h2 className="meme--text bottom">{memeData.bottomText}</h2>
+                </div>
+
 
                 <button type="submit" className="btn color-btn" onClick={getRandomImage}>Get new meme image</button>
             </div>
